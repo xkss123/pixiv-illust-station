36d6b5d601fd14fe06f0de120d60b579e6a47997
zjs <2785909653@qq.com>
2023-10-17 23:10:01 +0800

    修改了APP端和小程序端的登录方法

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -8,24 +8,44 @@
 	export default {
 		data() {
 			return {
-				
+
 			};
 		},
-		methods:{
-			login(){
+		methods: {
+			login() {
+				// #ifdef APP-PLUS 
 				uni.login({
-				  provider: 'weixin',
-				  success: function (loginRes) {
-				    console.log(loginRes.authResult);
-				    // 获取用户信息
-				    uni.getUserInfo({
-				      provider: 'weixin',
-				      success: function (infoRes) {
-				        console.log('用户昵称为：' + infoRes);
-				      }
-				    });
-				  }
+					provider: 'weixin',
+					success: function(loginRes) {
+						console.log(loginRes.authResult);
+						// 获取用户信息
+						uni.getUserInfo({
+							provider: 'weixin',
+							success: function(infoRes) {
+								console.log('用户昵称为：' + infoRes.userInfo.nickName);
+							},
+							fail: (err) => {
+								console.log(err);
+							}
+						});
+					},
+					fail: (err) => {
+						console.log(err);
+					}
 				});
+				// #endif
+				// #ifdef MP-WEIXIN
+				// TODO:因为微信小程序把API收回了只能返回默认头像和“微信用户”
+				uni.getUserProfile({
+					desc: 'Wexin', // 这个参数是必须的
+					success: res => {
+						console.log(res)
+					},
+					fail: err => {
+						console.log(err)
+					}
+				})
+				// #endif
 			}
 		}
 	}
